77a2bac883258b5b06ac3b25ddbc3afb85f0ebd4
Eimer Armijos <eaarmijos8@utpl.edu.ec>
2023-10-27 17:58:37 -0500

    ejercicio06/ Este comentario es de Eimer

---
+++ Ejercicio06/src/ejercicio06/Ejercicio06.java
@@ -0,0 +1,15 @@
+package ejercicio06;
+public class Ejercicio06 {
+    public static void main(String[] args) {
+        
+    boolean val01 = (Math.sqrt(81) + 9 / 3 == 9);
+    boolean val02 = (10>1);
+    boolean resultado1 = (val01);
+    boolean resultado2 = (val02);
+    
+        System.out.println("1er Resultado: " + resultado1);
+        System.out.println("2do Resultado: " + resultado2);
+    }
+    
+    
+}
