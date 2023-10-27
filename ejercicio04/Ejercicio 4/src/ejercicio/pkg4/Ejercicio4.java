b508d48904c931f795c9351d958df1f2e12bde1c
pablo <pablo.velezleon1@icloud.com>
2023-10-27 12:54:20 -0500

    ejercicio05/ este comentario es de Pablo

---
+++ ejercicio04/Ejercicio 4/src/ejercicio/pkg4/Ejercicio4.java
@@ -0,0 +1,17 @@
+
+package ejercicio.pkg4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ejercicio04/ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,17 @@
+
+package ejercicio05;
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        // Resolucion del problema
+        double var1 = Math.pow(2, 2);
+        int var2 = 12-1 / (3);
+        System.out.println("El resultado es: " + (var1+var2));
+        
+        
+        
+     
+     
+        
+    }
+    
+}=
