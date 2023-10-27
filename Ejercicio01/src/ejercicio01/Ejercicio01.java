850a966bfb449703d5d2a6bf1620c40b559f5eb5
Eimer Armijos <eaarmijos8@utpl.edu.ec>
2023-10-27 09:52:42 -0500

    este comentario es de eimer

--- Ejercicio01/src/ejercicio01/Ejercicio01.java
+++ Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 package ejercicio01;
-public class Ejercicio01 {
 
-
-=======
- 
-package ejercicio01;
 
 public class Ejercicio01 {
 
->>>>>>> f0ddd8099882c5ff26114b49144c820c1da77ed9
     public static void main(String[] args) {
         // Solución a la problemática
-
         int variable1 = 1;
         int variable2 = 2;
         int variable3 = 4;
@@ -24,7 +16,5 @@ public class Ejercicio01 {
         double resultado = (variable1 + variable2 + variable3 + variable4) / 
                 variable5 + Math.sqrt(variable6);
         System.out.println(resultado);
-
     }
-
 }
--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package ejercicio02;
 public class Ejercicio02 {
@@ -12,19 +11,6 @@ public class Ejercicio02 {
         respuesta = Math.pow(var1, 2) +var2;
         System.out.println("la respuesta es: " + respuesta);
         //Autor PabloVgit
-        
-                
-=======
-package ejercicio02;
-public class Ejercicio02{
-    public static void main(String[] args) {
-        // Resolución del problema
-        int var1 = 10;
-        int var2 = 10;     
-        double respuesta;
-        respuesta = Math.pow(var1, 2) + var2;
-        System.out.println("La respuesta es: " + respuesta );
-        //Autor Eimer Armijos
->>>>>>> fb93012a05c97e2f38efc2df07cb85070b070e76
+
     }
 }
