4fbc72bc20a55837417abb9e62ac1c5ebb604e07
pablo <pablo.velezleon1@icloud.com>
2023-10-28 08:47:25 -0500

    Ejercicio07 / este comentario es de pablo

---
+++ Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -0,0 +1,19 @@
+package ejercicio07;
+public class Ejercicio07 {
+    public static void main(String[] args) {
+        double var1 = Math.sqrt(81);
+        double var2 = 9 / (3);
+        int var3 = 10;
+        int var4 = 1;
+        double var5 = 100 / (25);
+        double var6 = Math.sqrt(100);
+        System.out.println("el resultado es: " + var1+var2);
+        System.out.println("el numero mayor es: " + Math.max(var3, var4));
+        System.out.println("segundo resultadoes: " + var5+var6);
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
